9db89c6ebf27af3faa2f5b263e7bd20bb77401d2
JavidGasim <143398895+JavidGasim@users.noreply.github.com>
2023-12-09 15:16:08 +0400

    Add files via upload

---
+++ StateDesignPattern_Task/StateDesignPattern_Task/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace StateDesignPattern_Task;
+
+interface LightState
+{
+    void Handle(Light context);
+}
+
+class OnState : LightState
+{
+    public void Handle(Light context)
+    {
+        Console.WriteLine("Light is already ON");
+    }
+}
+
+class OffState : LightState
+{
+    public void Handle(Light context)
+    {
+        Console.WriteLine("Turning ON the light");
+        context.SetState(new OnState());
+    }
+}
+
+class Light
+{
+    private LightState currentState;
+
+    public Light()
+    {
+        currentState = new OffState();
+    }
+
+    public void SetState(LightState state)
+    {
+        currentState = state;
+    }
+
+    public void PressSwitch()
+    {
+        currentState.Handle(this);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Light light = new Light();
+
+        light.PressSwitch(); 
+
+        light.PressSwitch(); 
+    }
+}
